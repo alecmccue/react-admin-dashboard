bcac4a11f085dc1a71adfe5f45659dc893ad47ef
Alec McCue <vgn2bh@virginia.edu>
2023-05-15 14:56:35 -0400

    mui theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+
 function App() {
+  const [theme, colorMode] = useMode();
   return <div className="app"></div>;
 }
 
